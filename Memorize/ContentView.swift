2463d6d52f53b52ebd9a206038ed508d92887510
Guillermo Haro <hhobbesg@gmail.com>
2024-05-07 20:43:17 -0600

    refactor: Show the cards using a LazyVGrid, adjust the card size and aspect ratio

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -13,16 +13,21 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            cards
+            ScrollView {
+                cards
+            }
+            Spacer()
             cardCountAdjusters
         }
         .padding()
     }
     
     var cards: some View {
-        HStack {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
             ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3,
+                                 contentMode: .fit)
             }
         }
         .foregroundStyle(.orange)
@@ -66,14 +71,15 @@ struct CardView: View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
             
-            if isFaceUp {
+            Group {
                 base.foregroundStyle(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content)
                     .font(.largeTitle)
-            } else {
-                base
             }
+            .opacity(isFaceUp ? 1 : 0)
+            
+            base.opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture {
             isFaceUp.toggle()
