163f12d4eef1ecbbdaa283d30b2df60a6ef32c64
Guillermo Haro <hhobbesg@gmail.com>
2024-05-07 19:35:08 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Flaizz on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
